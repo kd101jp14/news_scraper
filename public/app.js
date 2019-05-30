8b1a6cf667ad6e5f15f05508b6a4b1abc3aa0d85
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-30 03:09:01 -0500

    Updated 'app.js' so that when a user clicks on an article ('p' tag), a comment is able to be written for that article.

--- public/app.js
+++ public/app.js
@@ -14,3 +14,37 @@ $.getJSON("/articles", function(data) {
     );
   }
 });
+
+// Whenever someone clicks a p tag
+$(document).on("click", "p", function() {
+    // Empty the comments from the comment section
+    $("#comments").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the article
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // With that done, add the comment information to the page
+      .then(function(data) {
+        console.log(data);
+        // The title of the article
+        $("#comments").append("<h2>" + data.title + "</h2>");
+        // An input to enter a new title
+        $("#comments").append("<input id='titleinput' name='title' >");
+        // A textarea to add a new comment body
+        $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
+        // A button to submit a new comment, with the id of the article saved to it
+        $("#comments").append("<button data-id='" + data._id + "' id='savecomment'>Save comment</button>");
+  
+        // If there's a comment in the article
+        if (data.comment) {
+          // Place the title of the comment in the title input
+          $("#titleinput").val(data.comment.title);
+          // Place the body of the comment in the body textarea
+          $("#bodyinput").val(data.comment.body);
+        }
+      });
+  });
