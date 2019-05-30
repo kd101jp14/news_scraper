e83b37c2a5edfec161cc4e105cdadc94f8cfa476
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-30 02:15:17 -0500

    Added a route for saving and updating comments associated with articles.

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -60,5 +60,27 @@ module.exports = function(app) {
       });
   });
 
-  
+  // Route for saving/updating an Article's associated comment
+  app.post("/articles/:id", function(req, res) {
+    // Create a new comment and pass the req.body to the entry
+    db.Comment.create(req.body)
+      .then(function(dbComment) {
+        // If a comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the article to be associated with the new comment
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate(
+          { _id: req.params.id },
+          { comment: dbComment._id },
+          { new: true }
+        );
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 };
