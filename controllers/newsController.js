6623c57a2dad1e54b757caa7ba167316564271e0
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-05-30 23:12:04 -0500

    Updated 'app.js' and handlebars files so that the articles could be rendered to the UI.

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -9,84 +9,99 @@ var cheerio = require("cheerio");
 var db = require("./../models");
 
 // A GET route for scraping the positive.news website
+router.get("/", function(req, res) {
+  res.render("index", {name: "Kyra"});
+});
 
-  router.get("/", function(req, res) {
-    res.render("index");
-  });
+router.get("/scrape", function(req, res) {
+  // Grab the body of the html with axios
+  axios.get("https://www.positive.news/").then(function(response) {
+    // Load the data into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-  router.get("/scrape", function(req, res) {
-    // Grab the body of the html with axios
-    axios.get("https://www.positive.news/").then(function(response) {
-      // Load the data into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
+    // Grab every element with the class of `card__content`, and do the following:
+    $(".card__content").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
 
-      // Grab every element with the class of `card__content`, and do the following:
-      $(".card__content").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
-
-      // Send a message to the client
-      res.send("Scrape Complete");
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
     });
-  });
 
-  // Route for getting all Articles from the db
-  router.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
+    // Send a message to the client
+    res.send("Scrape Complete");
   });
+});
 
-  // Route for saving/updating an Article's associated comment
-  router.post("/articles/:id", function(req, res) {
-    // Create a new comment and pass the req.body to the entry
-    db.Comment.create(req.body)
-      .then(function(dbComment) {
-        // If a comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the article to be associated with the new comment
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return db.Article.findOneAndUpdate(
-          { _id: req.params.id },
-          { comment: dbComment._id },
-          { new: true }
-        );
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+// Route for getting all articles from the db
+router.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific article by id, populate it with it's comment
+router.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the comments associated with it
+    .populate("comment")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated comment
+router.post("/articles/:id", function(req, res) {
+  // Create a new comment and pass the req.body to the entry
+  db.Comment.create(req.body)
+    .then(function(dbComment) {
+      // If a comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the article to be associated with the new comment
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { comment: dbComment._id },
+        { new: true }
+      );
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
